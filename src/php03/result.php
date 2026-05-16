0dee50267ee215f7a9ea4c532a1b55658187acf1
おむつ <omuomu007@gmail.com>
2026-05-17 06:58:04 +0900

    quiz

--- src/php03/result.php
+++ src/php03/result.php
@@ -1,3 +1,26 @@
+<?php
+
+require_once('config/status_codes.php');
+
+$answer_code = isset($_POST['answer_code']) ? htmlspecialchars($_POST['answer_code'], ENT_QUOTES) : null;
+$option = isset($_POST['option']) ? htmlspecialchars($_POST['option'], ENT_QUOTES) : null;
+
+if (empty($option)) {
+    header('Location: index.php');
+    exit;
+}
+
+
+foreach ($status_codes as $status_code) {
+    if ($status_code['code'] === $answer_code) {
+        $code = $status_code['code'];
+        $description = $status_code['description'];
+    }
+}
+
+$result = $option === $code;
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -12,34 +35,41 @@
 </head>
 
 <body>
-  <header class="header">
-    <div class="header__inner">
-      <a class="header__logo" href="/">
-        Status Code Quiz
-      </a>
-    </div>
-  </header>
+<header class="header">
+  <div class="header__inner">
+    <a class="header__logo" href="/">
+      Status Code Quiz
+    </a>
+  </div>
+</header>
 
-  <main>
-    <div class="result__content">
-      <div class="result">
-        <h2 class="result__text--correct">正解</h2>
-        <h2 class="result__text--incorrect">不正解</h2>
-      </div>
-      <div class="answer-table">
-        <table class="answer-table__inner">
-          <tr class="answer-table__row">
-            <th class="answer-table__header">ステータスコード</th>
-            <td class="answer-table__text"></td>
-          </tr>
-          <tr class="answer-table__row">
-            <th class="answer-table__header">説明</th>
-            <td class="answer-table__text"></td>
-          </tr>
-        </table>
-      </div>
+<main>
+  <div class="result__content">
+    <div class="result">
+      <?php if ($result): ?>
+      <h2 class="result__text--correct">正解</h2>
+      <?php else: ?>
+      <h2 class="result__text--incorrect">不正解</h2>
+      <?php endif; ?>
     </div>
-  </main>
+    <div class="answer-table">
+      <table class="answer-table__inner">
+      <tr class="answer-table__row">
+        <th class="answer-table__header">ステータスコード</th>
+        <td class="answer-table__text">
+        <?php echo $code ?>
+        </td>
+      </tr>
+      <tr class="answer-table__row">
+        <th class="answer-table__header">説明</th>
+        <td class="answer-table__text">
+        <?php echo $description ?>
+      </td>
+      </tr>
+      </table>
+    </div>
+  </div>
+</main>
 </body>
 
 </html>
